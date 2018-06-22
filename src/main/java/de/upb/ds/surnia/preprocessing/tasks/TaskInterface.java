16de41c54427dabd5195ef71a358b4685616617b
Simon Bordewisch <sbordewisch@gmx.de>
2018-06-22 16:14:23 +0200

    Add javadoc to Taskinterface

--- src/main/java/de/upb/ds/surnia/preprocessing/tasks/TaskInterface.java
+++ src/main/java/de/upb/ds/surnia/preprocessing/tasks/TaskInterface.java
@@ -3,6 +3,11 @@ package de.upb.ds.surnia.preprocessing.tasks;
 import de.upb.ds.surnia.preprocessing.model.Token;
 import java.util.List;
 
+/**
+ * A task-object that processes the given question and the given tokens from previous tasks and
+ * produces its own list of tokens, depending on the implementation. Implementations should consider
+ * the previous token list as much as possible.
+ */
 public interface TaskInterface {
 
   List<Token> processTokens(String question, List<Token> tokens);
